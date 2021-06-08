a76b8352490433ebf281b9a08caf4e56faf83c8a
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-06-08 21:18:52 +0530

    First level commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java Training");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
